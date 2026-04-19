cbdaa8c96c5c391b79d0f937fad896103b1c2d87
tashmacmay <tash.macmay@gmail.com>
2026-04-19 14:13:40 +0200

    Fixed forEach, badge count & cartloading

--- script.js
+++ script.js
@@ -10,10 +10,9 @@ const initApp = () => { //Called when the application starts, therefore product 
     fetch('products.json')
     .then(response => response.json())
     .then(data => {
-        listProducts = data;
-        addDataToHTML();
+        listProducts = data; //data from json is added to the products array ^^^
 
-        //Restore data from local memory on app startup
+        //Restore data from local memory on app startup (if it is found)
         if(localStorage.getItem('cart')){
             cart = JSON.parse(localStorage.getItem('cart'));
             addCartToHTML(); //Shows data on screen
@@ -37,24 +36,24 @@ const addToCart = (product_id) => {
     }else{
         cart[positionThisProductInCart].quantity = cart[positionThisProductInCart].quantity + 1;
     }
-    addCartToHTML();
-    addCartToMemory();
+    addCartToHTML(); //The cart content is laid out on the cart page
+    addCartToMemory(); //The cart is save to the local storage for PERSISTENCY!
 }
 
 const addCartToHTML = () => {
     let totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0);
     iconCartBadge.innerText = totalQuantity; //Reliably updates cart badge every time Add to Cart is clicked
-    if(!CartHTML) return; //The method will end here in the event that the cart page's container does not exist
+    if(!CartHTML) return; //The method will end here in the event that the cart page's container does not exist (avoid crash)
 
     CartHTML.innerHTML = '';
     //let totalQuantity = 0;
     if(cart.length > 0){
-        cart.forEach(cart => {
-            totalQuantity = totalQuantity + cart.quantity;
+        cart.forEach(cartItem => { //cart is array and cartItem is the current item in array
+            //totalQuantity = totalQuantity + cart.quantity;
             let newCart = document.createElement('div');
             newCart.classList.add('cart-item');
-            newCart.dataset.id = cart.product_id;
-            let positionProduct = listProducts.findIndex((value) => value.id == cart.product_id)
+            newCart.dataset.id = cartItem.product_id;
+            let positionProduct = listProducts.findIndex((value) => value.id == cartItem.product_id)
             let info = listProducts[positionProduct];
             newCart.innerHTML = `
             <div class="item-img">
@@ -64,35 +63,37 @@ const addCartToHTML = () => {
                 ${info.name}
             </div>
             <div class="item-total-price">
-                ${info.price * cart.quantity}
+                ${info.price * cartItem.quantity}
             </div>
-            <div class="item-quantity" data-id="${cart.product_id}">
+            <div class="item-quantity" data-id="${cartItem.product_id}">
                 <span class="minus"><</span>
-                <span>${cart.quantity}</span>
+                <span>${cartItem.quantity}</span>
                 <span class="plus">></span>
             </div>
             `;
         CartHTML.appendChild(newCart);
         })
     }
-    iconCartBadge.innerText = totalQuantity;
+    //iconCartBadge.innerText = totalQuantity;
 }
 
 const addCartToMemory = () => {
     localStorage.setItem('cart', JSON.stringify(cart)); //save to local storage (can only store string, not array, so we "stringify")
 }
 
-CartHTML.addEventListener('click', (event) => {
-    let positionClick = event.target; //Note what the user has clicked
-    if(positionClick.classList.contains('minus') || positionClick.classList.contains('plus')){
-        let product_id = positionClick.parentElement.dataset.id;
-        let type = 'minus'; //Must determine whether to add or subtract - we assume subtract by default
-        if(positionClick.classList.contains('plus')){ //check whether plus, and if so, change accordingly
-            type = 'plus'
+if(CartHTML) {
+    CartHTML.addEventListener('click', (event) => {
+        let positionClick = event.target; //Note what the user has clicked
+        if(positionClick.classList.contains('minus') || positionClick.classList.contains('plus')){
+            let product_id = positionClick.parentElement.dataset.id;
+            let type = 'minus'; //Must determine whether to add or subtract - we assume subtract by default
+            if(positionClick.classList.contains('plus')){ //check whether plus, and if so, change accordingly
+                type = 'plus'
+            }
+            changeQuantity(product_id, type);
         }
-        changeQuantity(product_id, type);
-    }
-})
+    })
+}
 
 const changeQuantity = (product_id, type) => {
     let positionItemInCart = cart.findIndex((value) => value.product_id == product_id);
